021b88bc567e49fc98cfdd51061b5bf8c344c329
Matheus Moraes <matheus.moraes111@outlook.com>
2020-08-12 19:24:32 -0300

    Nodemon added and 404 error handled

--- src/app.js
+++ src/app.js
@@ -7,4 +7,19 @@ const ordersRouter = require('./routers/orders')
 app.use('/products', productsRouter)
 app.use('/orders', ordersRouter)
 
+app.use((req, res, next) => {
+    const error = new Error('Not found')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    return res.send({
+        error: {
+            'message': error.message
+        }
+    })
+})
+
 module.exports = app
