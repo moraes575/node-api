b6493c1b34516b2ea51241a7af8d63ef404313e1
Matheus Moraes <matheus.moraes111@outlook.com>
2020-08-15 16:46:29 -0300

    Improving responses

--- src/routes/products.js
+++ src/routes/products.js
@@ -10,7 +10,12 @@ router.get('/:id', (req, res, next) => {
             [req.params.id],
             (error, result, fields) => {
                 if (error) { return res.status(500).send({ error: error }) }
-                return res.status(200).send({ product: result })
+                if (result.length === 0) {
+                    return res.status(404).send({
+                        message: `Product not found. ID: ${req.params.id}`
+                    })
+                }
+                return res.status(200).send(result[0])
             }
         )
     })
@@ -23,7 +28,20 @@ router.get('/', (req, res, next) => {
             'SELECT * FROM products',
             (error, result, fields) => {
                 if (error) { return res.status(500).send({ error: error }) }
-                return res.status(200).send({ products: result })
+                const response = {
+                    quantity: result.length,
+                    products: result.map(product => {
+                        return {
+                            id: product.id,
+                            name: product.name,
+                            price: product.price,
+                            links: {
+                                href: 'http://localhost:3000/products/' + product.id
+                            }
+                        }
+                    })
+                }
+                return res.status(200).send(response)
             }
         )
     })
@@ -38,10 +56,17 @@ router.post('/', (req, res, next) => {
             (error, result, field) => {
                 conn.release()
                 if (error) { return res.status(500).send({ error: error }) }
-                res.status(201).send({
-                    message: 'Product created',
-                    product_id: result.insertId
-                })
+                const response = {
+                    product: {
+                        id: result.insertId,
+                        name: req.body.name,
+                        price: req.body.price,
+                        links: {
+                            href: 'http://localhost:3000/products/' + result.insertId
+                        }
+                    }
+                }
+                return res.status(201).send(response.product)
             }
         )
     })
@@ -56,9 +81,18 @@ router.put('/:id', (req, res, next) => {
             (error, result, field) => {
                 conn.release()
                 if (error) { return res.status(500).send({ error: error }) }
-                res.status(200).send({
-                    message: 'Product updated'
-                })
+                if (result.affectedRows === 0) { return res.status(404).send({ message: `Product not found. ID: ${req.params.id}` }) }
+                const response = {
+                    product: {
+                        id: req.params.id,
+                        name: req.body.name,
+                        price: req.body.price,
+                        links: {
+                            href: 'http://localhost:3000/products/' + req.params.id
+                        }
+                    }
+                }
+                return res.status(200).send(response.product)
             }
         )
     })
@@ -73,7 +107,8 @@ router.delete('/:id', (req, res, next) => {
             (error, result, field) => {
                 conn.release()
                 if (error) { return res.status(500).send({ error: error }) }
-                res.status(204).send()
+                if (result.affectedRows === 0) { return res.status(404).send({ message: `Product not found. ID: ${req.params.id}` }) }
+                return res.status(204).send()
             }
         )
     })
